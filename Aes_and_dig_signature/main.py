9283e966b8465d3e8d424b66b9dd62a331b51ce3
student283803 <283803@student.pwr.edu.pl>
2025-06-06 14:38:26 +0200

    aktualizacja

--- Aes_and_dig_signature/main.py
+++ Aes_and_dig_signature/main.py
@@ -3,18 +3,18 @@ from AES_decryption import decryption
 import digital_signing_file as dsf
 
 def main():
-    file_to_encrypt = "data.txt"
+    file_to_encrypt = input("Enter the file to encrypt: ")
     decrypted_file = "decrypted_data.txt"
     encrypted_file = "encrypted_data.bin"
     key_file = "key_file.bin"
     nonce_file = "nonce.bin"
     tag_file = "tag.bin"
-    file_to_sign = "data2.txt"
+    file_to_sign = input("Enter the file to sign: ")
     private_key_file = "private_key_file.pem"
     public_key_file = "public_key_file.pem"
     signature_file = "signature.sig"
 
-    """encryption(
+    encryption(
         file_to_encrypt,
         encrypted_file,
         key_file,
@@ -28,7 +28,7 @@ def main():
         tag_file,
         decrypted_file
     )
-"""
+
     dsf.generate_keys(private_key_file, public_key_file)
     dsf.sign_file(file_to_sign, private_key_file, signature_file)
     dsf.verify_signature(file_to_sign, signature_file, public_key_file)
